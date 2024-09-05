39d9b9c4ed8e0104d1aaee737c64840ba9081cbe
Joshua Isom <110069928+JoshIsomLehigh@users.noreply.github.com>
2024-09-05 16:41:39 -0400

    Update test_graph_representation_7.py

--- test/test_graph_representation_7.py
+++ test/test_graph_representation_7.py
@@ -1,10 +1,10 @@
-import subprocess
-import re
-from student_code import part_4_graph 
+import pytest
 
-# Test 3: Check for unhandled pylint style recommendations
-def test_part_4():
-       graph=part_4_graph()
-       assert type(graph)==dict
-       assert type(graph['a'])==set
-       assert graph['a']=={'a','b','e'}
+@pytest.fixture
+def graph():
+    from student_graph import VersatileDigraph
+    return VersatileDigraph()
+
+def test_add_edge_with_new_nodes(graph):
+    graph.add_edge("A", "B", edge_weight=5)
+    assert set(graph.get_nodes()) == {"A", "B"}
