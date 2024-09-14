9541459503b2eea0b22388961e6806169d69ddd0
kushalgowdagv <104550287+kushalgowdagv@users.noreply.github.com>
2024-09-14 19:54:42 -0400

    "first draft of code"

--- student_code.py
+++ student_code.py
@@ -8,7 +8,11 @@ class VersatileDigraph:
 
     def add_edge(self, start_node_id, end_node_id, start_node_value=0, end_node_value=0, edge_name=None, edge_weight=0):
         """Add an edge between two nodes."""
-        pass
+        if start_node_id not in self.nodes:
+            self.add_node(start_node_id, start_node_value)
+        if end_node_id not in self.nodes:
+            self.add_node(end_node_id, end_node_value)
+        self.edges[start_node_id][edge_name] = (end_node_id, edge_weight)
     def get_nodes(self):
         """Return a list of nodes in the graph."""
         return list(self.nodes.keys())
@@ -25,7 +29,10 @@ class VersatileDigraph:
 
     def get_edge_weight(self, start_node, end_node):
         """Return the weight of the edge between two nodes."""
-        pass
+        if start_node in self.edges:
+            for edge_name, (dest, weight) in self.edges[start_node].items():
+                if dest == end_node:
+                    return weight
     
     def print_graph(self):
         """Print the graph as a output."""
