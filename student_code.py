fdb380a605b2a69f6795fc67b0a429734e55c657
kushalgowdagv <104550287+kushalgowdagv@users.noreply.github.com>
2024-09-14 19:53:39 -0400

    "Initital node adding and configuration "

--- student_code.py
+++ student_code.py
@@ -2,22 +2,32 @@
 class VersatileDigraph:
     '''Class for versatile digraph implementation'''
     def __init__(self):
-        pass
+        """Initialization"""
+        self.nodes = {}
+        self.edges = {}
 
-    def add_edge():
+    def add_edge(self, start_node_id, end_node_id, start_node_value=0, end_node_value=0, edge_name=None, edge_weight=0):
+        """Add an edge between two nodes."""
         pass
+    def get_nodes(self):
+        """Return a list of nodes in the graph."""
+        return list(self.nodes.keys())
 
-    def get_nodes():
+    
+    def add_node(self, node_id, node_value=0):
+        """Return a list of nodes in the graph."""
+        if node_id not in self.nodes:
+            self.nodes[node_id] = node_value
+    
+    def get_node_value(self, node_id):
+        """Return the value of the node."""
+        return self.nodes.get(node_id, None)
+
+    def get_edge_weight(self, start_node, end_node):
+        """Return the weight of the edge between two nodes."""
         pass
     
-    def add_node():
-        pass
-    
-    def get_node_value():
-        pass
-
-    def get_edge_weight():
-        pass
-    
-    def print_graph():
-        pass
+    def print_graph(self):
+        """Print the graph as a output."""
+        for node_id, node_value in self.nodes.items():
+            print(f"Node {node_id} with value {node_value}")
