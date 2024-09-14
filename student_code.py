693d3cf90ec184c06233226ab5726635a2ce1084
kushalgowdagv <104550287+kushalgowdagv@users.noreply.github.com>
2024-09-14 19:50:05 -0400

    " initial Function "

--- student_code.py
+++ student_code.py
@@ -1,3 +1,23 @@
 
-class VersatileDigraph()
-    pass
+class VersatileDigraph:
+    '''Class for versatile digraph implementation'''
+    def __init__(self):
+        pass
+
+    def add_edge():
+        pass
+
+    def get_nodes():
+        pass
+    
+    def add_node():
+        pass
+    
+    def get_node_value():
+        pass
+
+    def get_edge_weight():
+        pass
+    
+    def print_graph():
+        pass
